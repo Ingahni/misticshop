8d3fbdc69fc92e591252d0b991b68126e4fd28c2
Inhagni <inhagni@gmail.com>
2025-04-30 11:43:13 +0200

    initial commit

---
+++ elit/main/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Category, Product
+
+
+def popular_list(request):
+    # categories = Category.objects.all()
+    products = Product.objects.filter(available=True)[:3]
+    return render(request, 
+                  'main/index/index.html',
+                  {'products': products})
+    
+def product_detail(request, slug):
+    product = get_object_or_404(Product, 
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'main/product/detail.html',
+                  {'product': product})   
+    # словарь с данными для шаблона
+    # в качестве значения передаем объект product 
+    # ключ product может называться как угодно
+    # в шаблоне мы обращаемся к объекту product
+    
+def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 1)
+    current_page = paginator.page(int(page))
+    
+    
+    if category_slug:
+        category = get_object_or_404(Category, 
+                                     slug=category_slug)
+        paginator = Paginator(products.filter(category=category), 1)
+        current_page = paginator.page(int(page))
+        # products = products.filter(category=category)
+        
+    return render(request,
+                  'main/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': current_page,
+                   'slug': category_slug})
+
